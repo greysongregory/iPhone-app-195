a28fff01c0423a29d441c0e81f75b343c14844f3
Vin <Vin@Vins-Mac.local>
2011-12-03 22:12:26 -0800

    first

---
+++ iphone-app/iphone-app/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  iphone-app
+//
+//  Created by Vin on 12/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
