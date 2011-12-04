3c021577536cdddbf55387323ab5595aa2279fa6
Vin <Vin@Vins-Mac.local>
2011-12-03 16:04:15 -0800

    new

---
+++ IPhone-app/IPhone-appTests/IPhone_appTests.h
@@ -0,0 +1,13 @@
+//
+//  IPhone_appTests.h
+//  IPhone-appTests
+//
+//  Created by Vin on 12/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IPhone_appTests : SenTestCase
+
+@end
---
+++ IPhone-app/IPhone-appTests/IPhone_appTests.m
@@ -0,0 +1,32 @@
+//
+//  IPhone_appTests.m
+//  IPhone-appTests
+//
+//  Created by Vin on 12/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "IPhone_appTests.h"
+
+@implementation IPhone_appTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in IPhone-appTests");
+}
+
+@end
